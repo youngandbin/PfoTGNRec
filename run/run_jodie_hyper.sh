f6f6c015dc07dd3a54463d01f4a929450f37e6ad
YOUNG <young@unist.ac.kr>
2025-07-07 21:41:47 +0900

    Update run_jodie_hyper.sh

--- run/run_jodie_hyper.sh
+++ run/run_jodie_hyper.sh
@@ -2,13 +2,13 @@
 lambda_mvs=(0.5)
 batch_sizes=(128 256 512)
 drop_outs=(0)
-n_headss=(2)
+n_heads=(2)
 
 # Iterate over each combination of hyperparameters
 for lambda_mv in "${lambda_mvs[@]}"; do
   for bs in "${batch_sizes[@]}"; do
     for drop_out in "${drop_outs[@]}"; do
-      for n_heads in "${n_headss[@]}"; do
+      for n_head in "${n_heads[@]}"; do
         # Get current time for each iteration
         current_time=$(TZ="Asia/Seoul" date -d "+0 hours" "+%y%m%d_%H%M%S")
         
@@ -20,7 +20,7 @@ for lambda_mv in "${lambda_mvs[@]}"; do
         --lambda_mv $lambda_mv \
         --bs $bs \
         --drop_out $drop_out \
-        --n_heads $n_heads 
+        --n_head $n_head
       done
     done
   done
